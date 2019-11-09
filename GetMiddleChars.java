f56caa860be15946b8255f011e65773747cd428c
testoxide <testoxide@gmail.com>
2019-11-09 11:57:41 +0000

    First Katas

---
+++ GetMiddleChars.java
@@ -0,0 +1,15 @@
+/* You are going to be given a word. Your job is to return the middle character of the word.
+If the word's length is odd, return the middle character. If the word's length is even,
+return the middle 2 characters. */
+
+public class GetMiddleChars {
+    public static String getMiddle(String word) {
+
+        int iLength = word.length();
+
+        boolean isEven = (iLength % 2 == 0);
+        if (!isEven)
+
+
+    }
+}
---
+++ PlantGrowth.java
@@ -0,0 +1,25 @@
+/* Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed
+meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning
+of a day. We want to know when the height of the plant will reach a certain level.*/
+
+public class PlantGrowth {
+    public static int growingPlant(int upSpeed, int downSpeed, int desiredHeight){
+        int i = 0;
+        for (int startHeight = 0; startHeight <= desiredHeight; startHeight += upSpeed, i++){
+            if (startHeight != 0){
+            System.out.println("After day " + i + " --> " + startHeight);
+            if (startHeight==desiredHeight) break;
+            startHeight = startHeight-=downSpeed;
+            System.out.println("After night " + i + " --> " + startHeight);
+        }
+        }
+        return i;
+    }
+
+    public static void main (String args[]) {
+        //PlantGrowth pg = new PlantGrowth();
+        int x = PlantGrowth.growingPlant(100,10,910);
+        System.out.println(x);
+    }
+
+}
