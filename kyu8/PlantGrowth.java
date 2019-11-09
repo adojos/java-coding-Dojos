be87e95b4865a79f853e58bafa59baa3cc433233
testoxide <testoxide@gmail.com>
2019-11-09 12:12:16 +0000

    dojo kyu levels

--- kyu8/PlantGrowth.java
+++ kyu8/PlantGrowth.java
@@ -1,3 +1,4 @@
+package kyu8;
 /* Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed
 meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning
 of a day. We want to know when the height of the plant will reach a certain level.*/
@@ -17,7 +18,7 @@ public class PlantGrowth {
     }
 
     public static void main (String args[]) {
-        //PlantGrowth pg = new PlantGrowth();
+        //kyu8.PlantGrowth pg = new kyu8.PlantGrowth();
         int x = PlantGrowth.growingPlant(100,10,910);
         System.out.println(x);
     }
