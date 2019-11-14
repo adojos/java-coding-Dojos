6381ed80b9369499701ab200e7caa7e0a0b2f8e4
testoxide <testoxide@gmail.com>
2019-11-14 00:55:04 +0000

    Palindrome

---
+++ kyu7/ReversingWords.java
@@ -0,0 +1,38 @@
+/*Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"*/
+
+package kyu7;
+
+public class ReversingWords {
+
+    public static String reverseWords(final String original) {
+
+        StringBuilder tempSB = new StringBuilder();
+        StringBuilder finalSB = new StringBuilder();
+        char[] arrChar = original.toCharArray();
+
+        for (char tmpChar : arrChar) {
+            if (!Character.isSpaceChar(tmpChar)) {
+                tempSB.append(tmpChar);
+            } else {
+                if (tempSB.length() != 0) {
+                    finalSB.append(tempSB.reverse());
+                    tempSB.delete(0, tempSB.length());
+                    finalSB.append(tmpChar);
+                } else {
+                    finalSB.append(tmpChar);
+                }
+            }
+
+        }
+        if (!tempSB.toString().isEmpty()){finalSB.append(tempSB.reverse());}
+        return finalSB.toString();
+    }
+
+    public static void main(String args[]) {
+        System.out.println(ReversingWords.reverseWords("This is an example"));
+    }
+
+}
