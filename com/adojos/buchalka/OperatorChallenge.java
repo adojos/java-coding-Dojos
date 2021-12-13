6000678e7acdb574e48cba399a3b2357bacf7283
Tushar Sharma <automationdojos@gmail.com>
2021-12-13 13:59:20 +0000

    Refactored

--- com/adojos/buchalka/OperatorChallenge.java
+++ com/adojos/buchalka/OperatorChallenge.java
@@ -1,4 +1,22 @@
 package com.adojos.buchalka;
 
 public class OperatorChallenge {
+
+    public static void main(String args[]) {
+        double firstDouble = 20.00d;
+        double secondDouble = 80.00d;
+
+        double totalDouble = (firstDouble + secondDouble) * 100.00d;
+        System.out.println("Total is: " + totalDouble);
+
+        double remainderDouble = totalDouble % 40.00d;
+        System.out.println("Remainder is " + remainderDouble);
+
+        boolean isFour;
+        if (remainderDouble == 0) {
+            isFour = true;
+        } else {isFour = false;}
+        System.out.println("Remainder is 0? : " + isFour);
+    }
+
 }
--- com/adojos/buchalka/PlayerChallenge.java
+++ com/adojos/buchalka/PlayerChallenge.java
@@ -1,4 +1,36 @@
+
 package com.adojos.buchalka;
 
 public class PlayerChallenge {
+
+    public static void main(String[] args) {
+
+        int intGetHighScore;
+        intGetHighScore = calculateHighScorePos(1500);
+        displayHighScore("Tim",intGetHighScore);
+
+        intGetHighScore = calculateHighScorePos(900);
+        displayHighScore("John",intGetHighScore);
+
+        intGetHighScore = calculateHighScorePos(400);
+        displayHighScore("Ruby",intGetHighScore);
+
+    }
+
+    public static void displayHighScore(String strName, int intPos){
+
+        System.out.println(strName + " managed to get into position " + intPos + " on the Score Table");
+    }
+
+    public static int calculateHighScorePos (int intScore){
+
+        int intPosition = 0;
+        if (intScore>1000){intPosition = 1;}
+        else if (intScore>500 && intScore<1000){intPosition = 2;}
+        else if (intScore>100 && intScore<500){intPosition = 3;}
+        else if (intScore>100 && intScore<500){intPosition = 4;}
+        return intPosition;
+    }
+
+
 }
