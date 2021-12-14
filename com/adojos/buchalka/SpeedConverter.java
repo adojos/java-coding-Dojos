3d4499b62dd0e509e5d4aa2bd8c27c0477199af3
Tushar Sharma <automationdojos@gmail.com>
2021-12-14 16:52:28 +0000

    Byte converter class

--- com/adojos/buchalka/SpeedConverter.java
+++ com/adojos/buchalka/SpeedConverter.java
@@ -3,7 +3,9 @@ package com.adojos.buchalka;
 public class SpeedConverter {
 
     public static void main(String[] args) {
-
+        double miles = toMilesPerHour(10.5);
+        System.out.println("Miles " + miles);
+        printConversion(10.5);
     }
 
     public static long toMilesPerHour (double kilometersPerHour){
@@ -16,7 +18,7 @@ public class SpeedConverter {
 
         long milesPerHour = toMilesPerHour(kilometersPerHour);
         if (milesPerHour < 0){System.out.println("Invalid Value!");}
-        else System.out.println(kilometersPerHour + "km/h" + " = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        else System.out.println(kilometersPerHour + " km/h" + " = " + toMilesPerHour(kilometersPerHour) + " mi/h");
     }
 
 }
